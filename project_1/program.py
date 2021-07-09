4bf1f946b991a49b80d59ddcdcae5727ee9d7e5f
Anthony <anthonybeavermusic@gmail.com>
2021-07-08 19:24:26 -0700

    first_commit

---
+++ project_1/program.py
@@ -0,0 +1,8 @@
+# This program says hello and asks for my name
+
+print('Hello World!')
+print('What is your name?')
+myName = input()
+nameLen = len(myName)
+print(f'Great to meet you {myName}')
+print(f'The length of your name is: {nameLen}')
