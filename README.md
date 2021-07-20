d2094490894c55ce369df054bec0b950b3840bb5
Anthony <anthonybeavermusic@gmail.com>
2021-07-19 21:30:36 -0700

    new stuff

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# automate_the_boring_stuff
+# Automate the Boring Stuff
+
+This repo contains my projects from Automate the Boring Stuff
+
+### Note to self: type 'python3' in the terminal to make ubuntu use IDLE.
+
+Dependencies:
+pyperclip
--- project_1/program.py
+++ project_1/program.py
@@ -1,8 +1,15 @@
 # This program says hello and asks for my name
+import sys
+import pyperclip
 
-print('Hello World!')
-print('What is your name?')
-myName = input()
-nameLen = len(myName)
-print(f'Great to meet you {myName}')
-print(f'The length of your name is: {nameLen}')
+def hello():
+    print('Hello World!')
+    print('What is your name?')
+    myName = input()
+    nameLen = len(myName)
+    print(f'Great to meet you {myName}')
+    print(f'The length of your name is: {nameLen}')
+    print('Thanks for using the app! \nExiting')
+    sys.exit()
+
+hello()
